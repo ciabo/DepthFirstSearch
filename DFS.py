798754d1952b7960e38976ff5a059f24f900fa2f
Luca <lucaciabo@hotmail.it>
2017-06-14 00:03:39 +0200

    Initial commit

---
+++ DFS.py
@@ -0,0 +1,23 @@
+
+time=0
+
+def DFS(nodeList):
+    for node in nodeList:
+        node.setParent(None)
+    for node in nodeList:
+        if node.getColor()=="white":
+            DFSVisit(node)
+
+
+def DFSVisit(u):
+    global time
+    time+=1
+    u.setD(time)
+    u.setColor("gray")
+    for node in u.getSons():
+        if node.getColor=="white":
+            node.setParent(u)
+            DFSVisit(node)
+    u.setColor("black")
+    time+=1
+    u.setEndTime(time)
